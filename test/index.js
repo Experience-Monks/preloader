b41fc5bdf718e76ff308efc2cb6e6422bd3ebf00
Nick <nick@jam3.com>
2016-07-06 17:14:37 -0400

    Updated tests

--- test/index.js
+++ test/index.js
@@ -11,7 +11,15 @@ loader.on('complete',function() {
   var data = loader.get('test_data.json');
   console.log('all content loaded:',data.success);
 });
-loader.add('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4');
-loader.add('http://sandbox.thewikies.com/vfe-generator/images/big-buck-bunny_poster.jpg');
+loader.add('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4',{
+  onComplete: function() {
+    document.body.appendChild(loader.get('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'))
+  }
+});
+loader.add('http://sandbox.thewikies.com/vfe-generator/images/big-buck-bunny_poster.jpg',{
+  onComplete: function() {
+    document.body.appendChild(loader.get('http://sandbox.thewikies.com/vfe-generator/images/big-buck-bunny_poster.jpg'))
+  }
+});
 loader.add('test_data.json');
 loader.load();
